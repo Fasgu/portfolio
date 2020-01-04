002125bf1279150f8bcbc010e110775f496948d5
aguerra95ugas <aguerra95ugas@gmail.com>
2020-01-04 17:35:18 -0500

    Se asignó rutas a los tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AngularFontAwesomeModule} from 'angular-font-awesome';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { TabComponent } from './tab/tab.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TabComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    RouterModule.forRoot([
+      { path: '', component: AboutComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: 'experience', component: ExperienceComponent },
+      { path: 'projects', component: ProjectsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
