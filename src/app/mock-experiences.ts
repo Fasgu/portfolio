a86d0bf58267cff1ad8f2c2376c6829a9058e279
fasgu95 <aguerra95ugas@gmail.com>
2020-06-22 21:50:40 -0500

    Actualizando datos

--- src/app/mock-experiences.ts
+++ src/app/mock-experiences.ts
@@ -11,43 +11,36 @@ export const EXPERIENCES: Experience[] = [
   {
     id: 2,
     company: 'Asociación de Personas Afectadas por Tuberculosis (ASPAT- PERÚ)',
-    period: 'Junio 2017 – Setiembre 2017',
-    position: 'Asistente de Sistemas',
-    description: 'Desarrollo de un Sistema Integral de Administración para la Tuberculosis (eVOTs) proyecto SIAT-TB'
-  },
-  {
-    id: 3,
-    company: 'Asociación de Personas Afectadas por Tuberculosis (ASPAT- PERÚ)',
-    period: 'Setiembre 2017 – Agosto 2018',
+    period: 'Junio 2017 – Agosto 2018',
     position: 'Jefe de Sistemas',
     description: 'Desarrollo de un Sistema Integral de Administración para la Tuberculosis (eVOTs) proyecto SIAT-TB y el Sistema Gerencial de Vacunas (SIGVAC)'
   },
   {
-    id: 4,
+    id: 3,
     company: 'Pormayorpe S. A',
     period: 'Setiembre 2018 – Diciembre 2018',
     position: 'Programador Informático',
     description: 'Colaboradora en el desarrollo de la plataforma pormayor.pe, megamercado de mayorista virtual'
   },
   {
-    id: 5,
+    id: 4,
     company: 'It Business Consultants Professionals S. A. C.',
-    period: 'Noviembre 2018 –Mayo 2019',
+    period: 'Noviembre 2018 – Mayo 2019',
     position: 'Analista / Programador',
     description: 'Desarrollo del sistema web con información analítica y estadística de ventas de estaciones de servicio (WebActiva)'
   },
   {
-    id: 6,
+    id: 5,
     company: 'R M Soluciones Perú S. A. C',
-    period: 'Febrero 2019 – Mayo 2019',
-    position: 'Programador Freelance',
-    description: 'Colaboradora en el desarrollo de sistemas de facturación electrónica.'
+    period: 'Marzo 2019 – Enero 2020',
+    position: 'Full Stack Developer',
+    description: 'Colaboradora en el desarrollo de sistemas de facturación electrónica'
   },
   {
-    id: 7,
-    company: 'R M Soluciones Perú S. A. C',
-    period: 'JUnio 2019 – Actualidad',
-    position: 'Software Director',
-    description: 'Coordinar, desarrollar un ERP que cuenta con facturación electrónica'
+    id: 6,
+    company: 'Senda TI Solutions SAC',
+    period: 'Enero 2020 – Actualidad',
+    position: 'Analista/Programador',
+    description: 'Colaboradora en desarrollo de sistemas (ERP)'
   },
 ];
